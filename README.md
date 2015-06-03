b5f33def9dd374fd760c70be7fa5838344c1fa3b
Valentin Podkameny <valentin@sethq.com>
2015-06-03 11:41:05 +0300

    updated gitmodules

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Greylock
 The Greylock Library is a simple and lightweight data visualization library.
+
+
+# Dependency
+git submodule add git@github.com:Datamart/Glize.git glize
+git submodule update --init --recursive
--- build/build.sh
+++ build/build.sh
@@ -3,7 +3,8 @@
 # http://google-styleguide.googlecode.com/svn/trunk/shell.xml
 
 readonly CWD=$(cd $(dirname $0); pwd)
-readonly GLIZE_PATH="${CWD}/../src/glize"
+readonly GLIZE_PATH="${CWD}/../glize"
+readonly GLIZE_REPO="git@github.com:Datamart/Glize.git"
 
 #
 # Prints message.
@@ -19,11 +20,18 @@ function println() {
 #
 function main() {
   println "[SYNC] ${GLIZE_PATH}:"
-  mkdir -p ${GLIZE_PATH}
-  cd ${GLIZE_PATH}
-  #git pull
-  git submodule update --init --recursive
-  cd ${CWD}
+  if [ -d "$DIRECTORY" ]; then
+    println "-- [REMOVE] ${GLIZE_PATH}"
+    git rm -r --force --cached ${GLIZE_PATH}
+  fi
+  println "-- [ADD] ${GLIZE_PATH}"
+  git submodule add --force ${GLIZE_REPO} ${GLIZE_PATH}
+  println "-- [UPDATE] ${GLIZE_PATH}"
+  git submodule update #--init --recursive
+  #git submodule foreach git pull
+
+  mkdir -p "${CWD}/../src/glize"
+  cp -r ${GLIZE_PATH}/src "${CWD}/../src/glize"
 
   println "[WEB] Running linter:"
   chmod +x jslint.sh && ./jslint.sh
