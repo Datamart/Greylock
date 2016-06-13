1481618d2285b78560fe89d172aa8923606926e4
Valentin <vpodk@users.noreply.github.com>
2016-06-13 23:01:48 +0300

    Updated jsdocs.

--- build/jsdoc.sh
+++ build/jsdoc.sh
@@ -1,34 +1,79 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
+#
+# Guide: https://google.github.io/styleguide/shell.xml
 # https://jsdoc-toolkit.googlecode.com/files/jsdoc_toolkit-2.4.0.zip
 # https://code.google.com/p/jsdoc-toolkit/wiki/CommandlineOptions
 
-JSDOC_JAR="./lib/jsdoc-toolkit/jsrun.jar"
-SRC_PATH="../src/charts"
-DOC_PATH="../docs"
-DOWNLOAD_URL=https://jsdoc-toolkit.googlecode.com/files/jsdoc_toolkit-2.4.0.zip
-WGET="`which wget`"
-CURL="`which curl`"
+
+readonly CWD=$(cd $(dirname $0); pwd)
+readonly LIB="${CWD}/lib"
+readonly TMP="${CWD}/tmp"
+
+readonly JSDOC_JAR="${LIB}/jsdoc-toolkit/jsrun.jar"
+readonly SRC_PATH="${CWD}/../src/charts"
+readonly DOC_PATH="${CWD}/../docs"
+readonly DOWNLOAD_URL=https://jsdoc-toolkit.googlecode.com/files/jsdoc_toolkit-2.4.0.zip
+
+readonly WGET="$(which wget)"
+readonly CURL="$(which curl)"
+readonly UNZIP="$(which unzip)"
+
+readonly JAVA="$(which java)"
+# Hot fix for clean installation of OS X El Capitan.
+readonly JAVA_OSX="/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/java"
 
 
-if [ ! -f "${JSDOC_JAR}" ]
-then
-    mkdir -p lib
-    rm -rf tmp && mkdir tmp && cd tmp
-    if [ -n "$WGET" ]; then
-        $WGET "${DOWNLOAD_URL}"
+#
+# Downloads jsdoc toolkit.
+#
+function download() {
+  if [[ ! -f "${JSDOC_JAR}" ]]; then
+    echo "Downloading jsdoc toolkit:"
+    mkdir -p "${LIB}"
+    rm -rf "${TMP}" && mkdir "${TMP}" && cd "${TMP}"
+    if [[ -n "$WGET" ]]; then
+      $WGET "${DOWNLOAD_URL}"
     else
-        $CURL "${DOWNLOAD_URL}" > ./jsdoc_toolkit-2.4.0.zip
+      $CURL "${DOWNLOAD_URL}" > ./jsdoc_toolkit-2.4.0.zip
     fi
-    unzip jsdoc_toolkit-2.4.0.zip -d ../lib
-    cd ../lib && mv jsdoc_toolkit*/jsdoc-toolkit ./
-    rm -rf jsdoc_toolkit*
-    cd ../ && rm -rf tmp
-fi
+    echo "Done"
 
-java -jar "${JSDOC_JAR}" ./lib/jsdoc-toolkit/app/run.js \
-     -r=5 "${SRC_PATH}" \
-     -t=./lib/jsdoc-toolkit/templates/jsdoc \
-     -d="${DOC_PATH}" \
-     -e=utf-8 \
-     --quiet
+    echo -n "Extracting jsdoc toolkit: "
+    $UNZIP -q "${TMP}/jsdoc_toolkit-2.4.0.zip" -d "${LIB}"
+    echo "Done"
+
+    cd "${LIB}" && mv jsdoc_toolkit*/jsdoc-toolkit ./
+    rm -rf jsdoc_toolkit*
+    cd "${CWD}" && rm -rf "${TMP}"
+  fi
+}
+
+#
+# Runs jsdoc toolkit.
+#
+function run() {
+  echo "Running jsdoc toolkit:"
+  local JAVA_BIN="${JAVA}"
+  if [[ -f "${JAVA_OSX}" ]]; then
+    JAVA_BIN="${JAVA_OSX}"
+  fi
+
+  "${JAVA_BIN}" -jar "${JSDOC_JAR}" "${LIB}/jsdoc-toolkit/app/run.js" \
+    -r=5 "${SRC_PATH}" \
+    -t="${LIB}/jsdoc-toolkit/templates/jsdoc" \
+    -d="${DOC_PATH}" \
+    -e=utf-8 \
+    --quiet
+
+  echo "Done"
+}
+
+#
+# The main function.
+#
+function main() {
+  download
+  run
+}
+
+main "$@"
